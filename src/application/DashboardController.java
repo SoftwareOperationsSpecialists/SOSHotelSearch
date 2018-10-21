e0a4e8df932ed529dbdc6889b957a026ef2a9f7d
Alejandro Sanchez <alexsanchez0512@yahoo.com>
2018-10-20 23:06:46 -0400

    review under construction

---
+++ src/application/DashboardController.java
@@ -0,0 +1,16 @@
+package application;
+
+public class DashboardController {
+
+  /*public void Hotel(ActionEvent event) throws Exception {
+
+    Parent Hotel = FXMLLoader.load(getClass().getResource("Hotel.fxml"));
+    Scene hotel = new Scene(Hotel);
+    //Goes to hotel search scene
+    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    window.setScene(hotel);
+    window.show();
+  }
+  */
+
+}
