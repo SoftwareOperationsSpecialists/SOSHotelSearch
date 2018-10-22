af7497c6896d29610b513b0beef703b7f29a19ff
Robbie <rwdarrow4582@eagle.fgcu.edu>
2018-10-22 00:56:16 -0400

    completed Hotel.fxml and HotelController.java

--- src/application/HotelController.java
+++ src/application/HotelController.java
@@ -8,15 +8,25 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import java.util.ArrayList;
 
 public class HotelController {
 
+  @FXML
+  private ImageView hotelPhotos;
 
+  private int numImages = 2;
+  private int imageArrayIndex = 0;
+  private ArrayList<Image> images = new ArrayList<>(numImages);
 
-
-
-
+  public HotelController(){
+    images.add(new Image("application/hotelpics/holiday-inn-the-colony-4629618286-16x5.jpg"));
+    images.add(new Image("application/hotelpics/room.jpg"));
+    images.add(new Image("application/hotelpics/holiday-inn-the-colony-4549822872-4x3.jpg"));
+  }
 
 
   // Book it button will open Payment information Scene
@@ -52,5 +62,23 @@ public class HotelController {
     window.show();
   }
 
+  public void NextImage(ActionEvent event) throws Exception {
+    try {
+      imageArrayIndex++;
+      hotelPhotos.setImage(images.get(imageArrayIndex));
+    } catch (IndexOutOfBoundsException e) {
+      hotelPhotos.setImage(images.get(0));
+      imageArrayIndex = 0;
+    }
+  }
 
+  public void PreviousImage(ActionEvent event) throws Exception {
+    try {
+      imageArrayIndex--;
+      hotelPhotos.setImage(images.get(imageArrayIndex));
+    } catch (IndexOutOfBoundsException e) {
+      hotelPhotos.setImage(images.get(images.size()-1));
+      imageArrayIndex = images.size()-1;
+    }
+  }
 }
