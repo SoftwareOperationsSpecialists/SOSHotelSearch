e3ef48d39228346912d61f265e42392ae97877ef
cwbicknese <42978071+cwbicknese@users.noreply.github.com>
2018-11-22 15:43:31 -0500

    Update DashController.java

--- src/application/DashController.java
+++ src/application/DashController.java
@@ -14,37 +14,38 @@ import javafx.scene.control.SpinnerValueFactory.IntegerSpinnerValueFactory;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
+// Sets up the dashboard where the user can begin their initial search
 public class DashController {
   @FXML
-  private TextField searchBar;
-  private static String location;
+  private TextField searchBar;          // TextField where user can type the location
+  private static String location;       // desired location of hotel
 
   @FXML
-  private MenuButton navigationBtn;
+  private MenuButton navigationBtn;     // button that starts the search
 
   @FXML
-  private MenuItem myAccountItem;
+  private MenuItem myAccountItem;       // allows the user to view their account
   @FXML
-  private MenuItem mySavedHotelItem;
+  private MenuItem mySavedHotelItem;    // allows the user to view their saved hotels
   @FXML
-  private MenuItem myReservationItem;
+  private MenuItem myReservationItem;   // allows the user to view their reservations
   @FXML
-  private MenuItem logoutItem;
+  private MenuItem logoutItem;          // allows the user to log out
 
   @FXML
-  private Spinner roomCount;
+  private Spinner roomCount;            // allows the user to toggle the amount of rooms they want
   private SpinnerValueFactory<Integer> roomCountFactory = new IntegerSpinnerValueFactory(0,9,1);
 
 
-//Side Panel buttons
+  //Side Panel buttons
   public void MyAccount(ActionEvent event) throws Exception {
-  Parent Logout = FXMLLoader.load(getClass().getResource("MyAccount.fxml"));
-  Scene logoutScene = new Scene(Logout);
+    Parent Logout = FXMLLoader.load(getClass().getResource("MyAccount.fxml"));
+    Scene logoutScene = new Scene(Logout);
 
-  Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-  window.setScene(logoutScene);
-  window.show();
-}
+    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    window.setScene(logoutScene);
+    window.show();
+  }
   public void logout(ActionEvent event) throws Exception {
     Parent Dashboard = FXMLLoader.load(getClass().getResource("Login.fxml"));
     Scene dashboard = new Scene(Dashboard);
@@ -53,19 +54,23 @@ public class DashController {
     window.setScene(dashboard);
     window.show();
   }
-
+  
+  // renders the search results based on the information given by the user when the search button is clicked
   public void Search(javafx.event.ActionEvent event) throws Exception {
-    location = searchBar.getText();
+    location = searchBar.getText();               // gets location from the searchBar TextField
 
     if (!location.isEmpty()) {
-      MapDriver mapDriver = new MapDriver();
+      // create map at given location
+      MapDriver mapDriver = new MapDriver();      
       mapDriver.setAddress(location);
       mapDriver.createMap();
-
+      
       if (mapDriver.getErrorStatus())   {
+        // error: prints error
         System.out.println(mapDriver.getError()); // this should be set to the text of a label
       }
       else {
+        // no error: goes to search results
         Parent Dashboard = FXMLLoader.load(getClass().getResource("Search.fxml"));
         Scene dashboard = new Scene(Dashboard);
         Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -84,7 +89,7 @@ public class DashController {
   }
 
   public void modifyRoom() {
-    this.roomCount.setValueFactory(roomCountFactory);
+    this.roomCount.setValueFactory(roomCountFactory);   // updates new roomCount value
   }
 
   public void changeScene(){
