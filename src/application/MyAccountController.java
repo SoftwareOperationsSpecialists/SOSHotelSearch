bdb7bd7cbfea44f3e84536a522cc6774f9d763ce
Alex <asanchezpadron0512@gmail.com>
2018-11-25 01:17:43 -0500

    Added button functionality that connects MyAccount scene with Reservation scene

--- src/application/MyAccountController.java
+++ src/application/MyAccountController.java
@@ -27,6 +27,15 @@ public class MyAccountController {
     window.setScene(savedScene);
     window.show();
   }
+
+  public void reservation(ActionEvent event) throws Exception {
+    Parent Dashboard = FXMLLoader.load(getClass().getResource("Reservations.fxml"));
+    Scene dashboard = new Scene(Dashboard);
+
+    Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
+    window.setScene(dashboard);
+    window.show();
+  }
   public void logout(ActionEvent event) throws Exception {
     Parent Dashboard = FXMLLoader.load(getClass().getResource("Login.fxml"));
     Scene dashboard = new Scene(Dashboard);
