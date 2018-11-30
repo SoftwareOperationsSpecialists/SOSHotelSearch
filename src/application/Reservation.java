8ab627f99b41cd3e7cf22cdc6bfa063a2cfcaefb
cwbicknese <42978071+cwbicknese@users.noreply.github.com>
2018-11-30 17:10:49 -0500

    Update Reservation.java

--- src/application/Reservation.java
+++ src/application/Reservation.java
@@ -5,6 +5,7 @@ import java.time.LocalDate;
 import java.time.temporal.ChronoUnit;
 
 public class Reservation {
+    //initialization
     private Hotel hotel;
     private long numOfNights;
     private int numOfRooms;
@@ -12,19 +13,21 @@ public class Reservation {
     private LocalDate checkInDate;
     private LocalDate checkOutDate;
 
+    //makes a reservation with the check-in date, checkout date, and number of rooms as parameters
     public Reservation(LocalDate checkInDate,
                        LocalDate checkOutDate, int numOfRooms) {
-        this.checkInDate = checkInDate;
-        this.checkOutDate = checkOutDate;
-        this.numOfRooms = numOfRooms;
-        numOfNights = ChronoUnit.DAYS.between(checkInDate, checkOutDate);
-        System.out.println(numOfNights);
+        this.checkInDate = checkInDate;     //check-in date
+        this.checkOutDate = checkOutDate;   //check-out date
+        this.numOfRooms = numOfRooms;       //number of rooms
+        numOfNights = ChronoUnit.DAYS.between(checkInDate, checkOutDate);   //gets the number of nights by finding the difference
+        System.out.println(numOfNights);                                    //  between the check-in and check-out dates
     }
 
     public Hotel getHotel() {
         return hotel;
     }
 
+    //sets the hotel to the current hotel and finds the final cost based on standard price, number of rooms, and nights
     public void setHotel(Hotel hotel) {
         this.hotel = hotel;
         finalCost = hotel.getStdPrice() * numOfRooms * numOfNights;
@@ -34,6 +37,7 @@ public class Reservation {
         return numOfNights;
     }
 
+    //sets number of nights to difference between check-in and check-out dates
     public void setNumOfNights(LocalDate checkInDate, LocalDate checkOutDate) {
       numOfNights = ChronoUnit.DAYS.between(checkInDate, checkOutDate);
     }
