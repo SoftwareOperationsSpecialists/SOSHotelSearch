78f5255da65217ad9f4d68eb69eb2c4ad1aaae7f
cwbicknese <42978071+cwbicknese@users.noreply.github.com>
2018-11-30 12:50:09 -0500

    Update LogInController.java

--- src/application/LogInController.java
+++ src/application/LogInController.java
@@ -10,28 +10,34 @@ import javafx.scene.paint.Paint;
 
 import java.sql.*;
 
+//verifies username and password and allows the user to login as a hotel searcher or hotel owner
 public class LogInController extends Credentials {
+  //initialization 
   @FXML
-  private Label lblStatus;
+  private Label lblStatus;    //writes if login fails
 
+  //username and password
   @FXML
   private TextField txtUsername;
   @FXML
   private TextField txtPassword;
 
+  //radio buttons to choose hotel searcher or hotel owner
   @FXML
   private RadioButton searcherBtn;
   @FXML
   private RadioButton ownerBtn;
 
   @FXML
-  private ToggleGroup loginType;
+  private ToggleGroup loginType;  
 
   //Action for login button
   public void Login(ActionEvent event) throws Exception {
 
+    //gets status of whether the user has chosen hotel searcher or hotel owner
     RadioButton selectedRadioButton = (RadioButton) loginType.getSelectedToggle();
 
+    //hotel searcher login
     if (selectedRadioButton == searcherBtn) {
       try {
         Class.forName(driver);
@@ -41,20 +47,24 @@ public class LogInController extends Credentials {
                 + txtUsername.getText() + "'" + " AND PASSWORD='" + txtPassword.getText() + "'";
         ResultSet result = statement.executeQuery(searcherSQL);
 
-        if (result.next()) {
-          Navigator.dashboard(event);
-          clientUsername = txtUsername.getText();
-          clientPassword = txtPassword.getText();
-          loginConnection.close();
-        } else {
-          lblStatus.setText("Login Failed");
-          lblStatus.setTextFill(Paint.valueOf("red"));
+        //login successful
+        if (result.next()) {                                
+          Navigator.dashboard(event);                     //go to dashboard scene
+          clientUsername = txtUsername.getText();         //gets username
+          clientPassword = txtPassword.getText();         //gets password
+          loginConnection.close();                        
+        } else {                                           
+          //login failed
+          lblStatus.setText("Login Failed");              //write login failed
+          lblStatus.setTextFill(Paint.valueOf("red"));    //  in red
           loginConnection.close();
         }
       } catch (SQLException ex) {
         System.out.println(ex);
 
       }
+      
+    //hotel owner login
     } else if (selectedRadioButton == ownerBtn) {
       try {
         Class.forName(driver);
@@ -64,14 +74,15 @@ public class LogInController extends Credentials {
         Statement statement = loginConnection.createStatement();
         ResultSet result = statement.executeQuery(ownerSQL);
 
+        //login successful
         if (result.next()) {
-          Navigator.dashboard(event);
+          Navigator.dashboard(event);                     //go to dashboard scene
           loginConnection.close();
+        //login failed
         } else {
-          lblStatus.setText("Login Failed");
-          lblStatus.setTextFill(Paint.valueOf("red"));
+          lblStatus.setText("Login Failed");              //write login failed
+          lblStatus.setTextFill(Paint.valueOf("red"));    //  in red
           loginConnection.close();
-          //To change scenes copy and paste this into if statement and enter the new fxml scene name into parent root
         }
       } catch (SQLException e) {
         e.printStackTrace();
@@ -81,7 +92,7 @@ public class LogInController extends Credentials {
 
   //Register button action
   public void SignUp(ActionEvent event) throws Exception {
-    Navigator.register(event);
+    Navigator.register(event);    //go to create account scene
   }
 
 }
