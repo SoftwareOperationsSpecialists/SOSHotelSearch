52fdb35bc704f83bdaf4f5d03af63a3ea4a63795
cwbicknese <42978071+cwbicknese@users.noreply.github.com>
2018-11-30 12:24:27 -0500

    Update HotelController.java

--- src/application/HotelController.java
+++ src/application/HotelController.java
@@ -11,35 +11,38 @@ import javafx.scene.text.Text;
 
 import java.util.ArrayList;
 
+//this class is for the hotel information when the user views a specific hotel
 public class HotelController {
 
   @FXML
-  private ImageView hotelPhotos;
+  private ImageView hotelPhotos;  //photos
   
   @FXML
-  private Text hotelName;
+  private Text hotelName;         //name
   @FXML
-  private Text hotelLocation;
+  private Text hotelLocation;     //location
   @FXML
-  private Text hotelStars;
+  private Text hotelStars;        //stars
   @FXML
-  private Text hotelPrice;
+  private Text hotelPrice;        //price
 
 
-  private int numImages = 2;
-  private int imageArrayIndex = 0;
-  private ArrayList<Image> images = new ArrayList<>(numImages);
+  private int numImages = 2;        //there will be three hotel images with indexes 0, 1, and 2
+  private int imageArrayIndex = 0;  //sets the image index to 0
+  private ArrayList<Image> images = new ArrayList<>(numImages); //array list of the images
   private static Hotel hotel;
 
   public void initialize(){
+    //creates the three images of the hotel
     images.add(new Image("application/hotelpics/holiday-inn-the-colony-4629618286-16x5.jpg"));
     images.add(new Image("application/hotelpics/room.jpg"));
     images.add(new Image("application/hotelpics/holiday-inn-the-colony-4549822872-4x3.jpg"));
 
-    hotelName.setText(hotel.getName());
-    hotelLocation.setText("Location: "+hotel.getCity()+", "+hotel.getCountryName());
-    hotelStars.setText("This is a "+ Hotel.getStars()+" star hotel.");
-    hotelPrice.setText("Price : $"+hotel.getStdPrice());
+    //initializes hotel info
+    hotelName.setText(hotel.getName());                                               //name of hotel
+    hotelLocation.setText("Location: "+hotel.getCity()+", "+hotel.getCountryName());  //location: city, country
+    hotelStars.setText("This is a "+ Hotel.getStars()+" star hotel.");                //stars
+    hotelPrice.setText("Price : $"+hotel.getStdPrice());                              //price
   }
   
   // Dashboard Button will go back to the "Hotel Search" Scene
@@ -57,13 +60,14 @@ public class HotelController {
 
     Navigator.payment(event);
 
+    //stores check-in and check-out dates in a string
     String insert_reservation ="INSERT INTO RESERVATIONS VALUES(" + reservation.getCheckInDate()
         + "," + reservation.getCheckOutDate() +")";
-
-        String insert_hotel = "INSERT INTO HOTEL VALUES(" + hotelName +","
-            + reservation.getFinalCost() +"," + Hotel.getStars() + "," + DashController.getLocation()
-            + "," + reservation.getNumOfRooms() +")";
-
+    
+    //stores hotel name, final cost, stars, location, and number of rooms in a string
+    String insert_hotel = "INSERT INTO HOTEL VALUES(" + hotelName +","
+        + reservation.getFinalCost() +"," + Hotel.getStars() + "," + DashController.getLocation()
+        + "," + reservation.getNumOfRooms() +")";
 
     try (Connection connection = DriverManager.getConnection(Credentials.url);
         Statement statement = connection.createStatement()) {
@@ -77,26 +81,29 @@ public class HotelController {
     Navigator.reviews(event);
   }
   
+  //allows user to view the next hotel image
   public void NextImage(){
     try {
-      imageArrayIndex++;
-      hotelPhotos.setImage(images.get(imageArrayIndex));
-    } catch (IndexOutOfBoundsException e) {
-      hotelPhotos.setImage(images.get(0));
+      imageArrayIndex++;                                  //increase image index by 1
+      hotelPhotos.setImage(images.get(imageArrayIndex));  //set image to its index
+    } catch (IndexOutOfBoundsException e) {               //if user increases index past 2
+      hotelPhotos.setImage(images.get(0));                //    it wraps back to 0
       imageArrayIndex = 0;
     }
   }
     
+  //allows user to view the previous hotel image
   public void PreviousImage(){
     try {
-      imageArrayIndex--;
-      hotelPhotos.setImage(images.get(imageArrayIndex));
-    } catch (IndexOutOfBoundsException e) {
-      hotelPhotos.setImage(images.get(images.size()-1));
-      imageArrayIndex = images.size()-1;
+      imageArrayIndex--;                                  //decrease image index by 1
+      hotelPhotos.setImage(images.get(imageArrayIndex));  //set image to its index
+    } catch (IndexOutOfBoundsException e) {               //if user decreases index below 0
+      hotelPhotos.setImage(images.get(images.size()-1));  //  it wraps to the number of images - 1
+      imageArrayIndex = images.size()-1;                  //  which brings the index to 2
     }
   }
     
+  //sets the hotel to the current hotel
   public static void setHotel(Hotel thisHotel) {
     hotel = thisHotel;
   }
