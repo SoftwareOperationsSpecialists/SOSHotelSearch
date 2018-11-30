7b24f56af30810d801f595448dacfcbc36d7ebed
cwbicknese <42978071+cwbicknese@users.noreply.github.com>
2018-11-30 16:44:50 -0500

    Update PayController.java

--- src/application/PayController.java
+++ src/application/PayController.java
@@ -4,18 +4,21 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+//functionality for payment scene
 public class PayController {
-@FXML
-private Label numberOfRooms;
-@FXML
-private Label price;
-@FXML
-private Label numberOfNights;
+  @FXML
+  private Label numberOfRooms;
+  @FXML
+  private Label price;
+  @FXML
+  private Label numberOfNights;
 
+  //go to thank you scene which confirms the reservation
   public void Pay(ActionEvent event) throws Exception {
     Navigator.thankYouScene(event);
   }
 
+  //go to hotel info scene
   public void HotelInfo(ActionEvent event) throws Exception {
     Navigator.hotelInfo(event);
   }
