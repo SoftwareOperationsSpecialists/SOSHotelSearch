a5a95d7ba3265a81026a873746b09d8ae74683e8
rwdarrow <rwdarrow4582@eagle.fgcu.edu>
2018-12-01 16:35:24 -0500

    added loading animation on dashboard

--- src/application/DashController.java
+++ src/application/DashController.java
@@ -1,11 +1,15 @@
 package application;
 
+import javafx.concurrent.Task;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.*;
 import javafx.scene.control.SpinnerValueFactory.IntegerSpinnerValueFactory;
 
+import java.io.IOException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
@@ -24,6 +28,9 @@ public class DashController implements Initializable {
     private Label searchStatus;
     @FXML
     private Spinner roomCount;
+
+    @FXML
+    private ProgressIndicator indicator;
     
     private SpinnerValueFactory<Integer> roomCountFactory = new IntegerSpinnerValueFactory(0, 9, 1);
     private static LocalDate userCheckInDate;
@@ -53,17 +60,47 @@ public class DashController implements Initializable {
             searchStatus.setText("Please select check in and check out date!");
 
         } else {
-            MapManager mapManager = new MapManager();
-            mapManager.setAddress(location);
-            mapManager.createMap();
-            userCheckInDate = checkInDate.getValue();
-            userCheckOutDate = checkOutDate.getValue();
-            numOfRooms = (int) roomCount.getValue();
-            Navigator.search(event);
+            // setup a new task for UI thread to run while loading the new scene
+            Task<Parent> loadAnimation = new Task<Parent>() {
+                @Override
+                public Parent call() throws IOException {
+                    searchStatus.setVisible(false);
+                    indicator.setVisible(true);
 
-            if (mapManager.getErrorStatus()) {
-                searchStatus.setText(mapManager.getError()); // this should be set to the text of a label
-            }
+                    // dummy data because the function needs to have the parent returned
+                    FXMLLoader loader = new FXMLLoader(getClass().getResource("Dashboard.fxml"));
+                    Parent root = loader.load();
+                    return root;
+                }
+            };
+
+            loadAnimation.setOnSucceeded(successEvent -> {
+                indicator.setVisible(false);
+                searchStatus.setVisible(true);
+                MapManager mapManager = new MapManager();
+                mapManager.setAddress(location);                // sets map location
+                mapManager.createMap();                         // creates map
+                userCheckInDate = checkInDate.getValue();       // gets check-in date
+                userCheckOutDate = checkOutDate.getValue();     // gets check-out date
+                numOfRooms = (int) roomCount.getValue();        // gets number of rooms
+
+                //checks for error
+                if (mapManager.getErrorStatus()) {
+                    searchStatus.setText(mapManager.getError());
+                } else {
+                    searchStatus.setVisible(false);
+                    try {
+                        Navigator.search(event);                      // go to search screen
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+
+            loadAnimation.setOnFailed(e -> loadAnimation.getException().printStackTrace());
+
+            Thread thread = new Thread(loadAnimation);
+            thread.start();
         }
     }
 
