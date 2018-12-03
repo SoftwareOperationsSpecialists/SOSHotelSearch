b8afe6182cfa9540ecc7adce457e433cb0785464
rwdarrow <32168203+rwdarrow@users.noreply.github.com>
2018-12-02 21:38:01 -0500

    added javadoc comments

--- src/application/Main.java
+++ src/application/Main.java
@@ -7,8 +7,17 @@ import javafx.scene.Scene;
 
 import javafx.stage.Stage;
 
+/**
+ * Desc: Main class for program that handles Application functions.
+ */
+
 public class Main extends Application {
 
+   /**
+    * Desc: intializes the JavaFX GUI and sets scene to Login
+    * @param: primaryStage - the stage for the GUI
+    * @throws: Exception
+    */
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("LogIn.fxml"));
@@ -17,6 +26,10 @@ public class Main extends Application {
         primaryStage.show();
     }
 
+   /**
+    * Desc: launches the program
+    * @param: args - array of Strings
+    */
     public static void main(String[] args) {
         launch(args);
     }
