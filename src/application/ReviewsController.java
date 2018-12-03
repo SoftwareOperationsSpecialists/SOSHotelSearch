6ae0d515583eb65c731d08c9341d5e353a086771
Alex <alexsanchez0512@yahoo.com>
2018-12-02 22:24:58 -0500

    review DB connection complete

--- src/application/ReviewsController.java
+++ src/application/ReviewsController.java
@@ -1,11 +1,18 @@
 package application;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
@@ -13,9 +20,54 @@ import javafx.stage.Stage;
 
 public class ReviewsController {
 
+  @FXML
+  TextField review;
+  @FXML
+  ChoiceBox<Integer> userRating;
+
+
+  String URL = Credentials.getUrl();
+  int hotelID = SearchController.hotelClicked.getHotelId();
+
+
+  String check_For_Table_SQL = " SELECT FROM REVIEW." + hotelID;
+
+  String create_Table_SQL = "CREATE TABLE " + hotelID
+      + "("
+      + "USERNAME varchar(255),"
+      + "REVIEW varchar(255),"
+      + "USER_RATING int,";
+
+  String create_Review_SQL = "INSERT INTO REVIEW." + hotelID
+      + " ( USERNAME, REVIEW, USER_RATING) "
+      + "VALUES ("
+      + "'" + Credentials.getClientUsername() + "', "
+      + "'" + review.getText() + "', "
+      + "'" + userRating.getSelectionModel().getSelectedItem() + "'"
+      + " )";
+
   public void submitButton(ActionEvent event) throws Exception {
     Navigator.hotelInfo(event);
+    try {
+      Class.forName(Credentials.getDriver());
+      Connection reviewConnection = DriverManager.getConnection(URL);
+      Statement statement = reviewConnection.createStatement();
+      ResultSet resultSet = statement.executeQuery(check_For_Table_SQL);
+
+      if (resultSet.next()) {
+        Statement stmt = reviewConnection.createStatement();
+        stmt.executeQuery(create_Review_SQL);
+      } else {
+        Statement stmt = reviewConnection.createStatement();
+        stmt.executeQuery(create_Table_SQL);
+      }
+
+    } catch (SQLException ex) {
+      System.out.println(ex);
+    }
+
   }
+
   public void backButton(ActionEvent event) throws Exception {
     Navigator.hotelInfo(event);
   }
