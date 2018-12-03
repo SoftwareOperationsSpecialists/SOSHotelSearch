685f9c6ac2cea2ee9c565a44b167076c4c0eab98
Allen Telson <all.telson@gmail.com>
2018-12-03 18:09:43 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	derby.log
    #	lib/SOSHotelAccountDB/db.lck
    #	lib/SOSHotelAccountDB/log/log.ctrl
    #	lib/SOSHotelAccountDB/log/log6.dat
    #	lib/SOSHotelAccountDB/log/logmirror.ctrl
    #	lib/SOSHotelAccountDB/seg0/c150.dat
    #	lib/SOSHotelAccountDB/seg0/c161.dat
    #	lib/SOSHotelAccountDB/seg0/c171.dat
    #	lib/SOSHotelAccountDB/seg0/c20.dat
    #	lib/SOSHotelAccountDB/seg0/c230.dat
    #	lib/SOSHotelAccountDB/seg0/c241.dat
    #	lib/SOSHotelAccountDB/seg0/c251.dat
    #	lib/SOSHotelAccountDB/seg0/c31.dat
    #	lib/SOSHotelAccountDB/seg0/c41.dat
    #	lib/SOSHotelAccountDB/seg0/c51.dat
    #	lib/SOSHotelAccountDB/seg0/c90.dat
    #	lib/SOSHotelAccountDB/seg0/ca1.dat
    #	lib/SOSHotelAccountDB/seg0/ca70.dat
    #	lib/SOSHotelAccountDB/seg0/caa0.dat
    #	lib/SOSHotelAccountDB/seg0/cb1.dat
    #	lib/SOSHotelAccountDB/seg0/cb10.dat

---
+++ src/application/Review.java
@@ -0,0 +1,48 @@
+package application;
+
+/**
+* Desc: allows the user to write reviews and give a rating to a hotel
+*/
+public class Review {
+
+  private String username;
+  private String review;
+  private String userRating;
+
+
+  /**
+  * Desc: sets the user, text, and rating for the review
+  * @param: username - the username of the user making the review
+  * @param: review - the text of the review
+  * @param: userRating - rating of hotel given by the user
+  */
+  public Review(String username, String review, String userRating) {
+    this.username = username;
+    this.review = review;
+    this.userRating = userRating;
+  }
+
+  /**
+  * Desc: gets the username of the user
+  * @return: username - the username of the user's account
+  */
+  public String getUsername() {
+    return username;
+  }
+
+  /**
+  * Desc: gets the text written for the review
+  * @return: review - the text the user writes for the review
+  */
+  public String getReview() {
+    return review;
+  }
+
+  /**
+  * Desc: gets the rating for the review
+  * @return: review - the rating the user gives to the hotel
+  */
+  public String getUserRating() {
+    return userRating;
+  }
+}
