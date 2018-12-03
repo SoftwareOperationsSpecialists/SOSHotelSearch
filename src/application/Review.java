0f2b5a670ce352690351c536714d38da174e3358
Alex <alexsanchez0512@yahoo.com>
2018-12-03 12:20:52 -0500

    Review Scene Works

--- src/application/Review.java
+++ src/application/Review.java
@@ -2,4 +2,26 @@ package application;
 
 public class Review {
 
+  private String username;
+  private String review;
+  private String userRating;
+
+
+  public Review(String username, String review, String userRating) {
+    this.username = username;
+    this.review = review;
+    this.userRating = userRating;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getReview() {
+    return review;
+  }
+
+  public String getUserRating() {
+    return userRating;
+  }
 }
--- src/application/ReviewsController.java
+++ src/application/ReviewsController.java
@@ -7,6 +7,9 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Observable;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -15,28 +18,46 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Paint;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 
 public class ReviewsController {
+
   @FXML
-  private StackPane reviewList;
+  private TextField Review;
   @FXML
-  TextField Review;
+  private ChoiceBox<String> userRating;
   @FXML
-  ChoiceBox<Integer> userRating;
+  private TableView reviewTable;
+  @FXML
+  private TableColumn usernameColumn;
+  @FXML
+  private TableColumn userRatingColumn;
+  @FXML
+  private TableColumn reviewColumn;
 
   String review;
-  Integer rating;
+  String rating;
 
   String URL = Credentials.getUrl();
   String hotelID = HotelController.getHotel().getHotelId();
 
+  static ObservableList<Review> reviewsList;
 
-  public void initialize(){
-    ArrayList<>
+
+  public void initialize() throws Exception {
+    updateList();
+
+
   }
 
   public void submitButton(ActionEvent event) throws Exception {
@@ -48,57 +69,103 @@ public class ReviewsController {
         + "VALUES ("
         + "'" + Credentials.getClientUsername() + "', "
         + "'" + review + "', "
-        + rating
+        + "'" + rating + "'"
         + " )";
 
     String create_Table_SQL = "CREATE TABLE REVIEW." + "\"" + hotelID + "\""
         + "("
         + "USERNAME varchar(255),"
         + "REVIEW varchar(255),"
-        + "USER_RATING int"
+        + "USER_RATING varchar(10) "
         + ")";
     try {
       Class.forName(Credentials.getDriver());
       Connection reviewConnection = DriverManager.getConnection(URL);
       Statement stmt = reviewConnection.createStatement();
       stmt.executeUpdate(create_Table_SQL);
-      System.out.println("Created new Table");
       reviewConnection.close();
       submitButton(new ActionEvent());
 
 
     } catch (SQLException ex) {
+      ex.printStackTrace();
       Class.forName(Credentials.getDriver());
       Connection reviewConnection = DriverManager.getConnection(URL);
       Statement stmt = reviewConnection.createStatement();
       stmt.executeUpdate(create_Review_SQL);
       reviewConnection.close();
-      System.out.println("Created Review");
+    }
+    initialize();
+  }
 
+  public void updateList() throws Exception{
+    reviewsList = FXCollections.observableArrayList();
+    try {
+      String get_review_SQL = "SELECT * FROM REVIEW." +"\"" + hotelID +"\"" ;
+
+      Connection getReviewConnection = DriverManager.getConnection(URL);
+      Statement statement = getReviewConnection.createStatement();
+      ResultSet resultSet = statement.executeQuery(get_review_SQL);
+
+      while (resultSet.next()) {
+
+        Review review = new Review(
+            resultSet.getString("USERNAME"),
+            resultSet.getString("REVIEW"),
+            resultSet.getString("USER_RATING"));
+        reviewsList.add(review);
+
+      }
+      usernameColumn.setCellValueFactory(new PropertyValueFactory<Review, String>("username"));
+      userRatingColumn.setCellValueFactory(new PropertyValueFactory<Review, String>("userRating"));
+      reviewColumn.setCellValueFactory(new PropertyValueFactory<Review, String>("review"));
+
+      reviewTable.getItems().setAll(reviewsList);
+    } catch (Exception ex) {
+      ex.printStackTrace();
+      System.out.println("No reviews");
     }
 
-//    try {
-//       review = Review.getText();
-//       rating = userRating.getSelectionModel().getSelectedItem();
+
+
+//    for (Review review: reviewsList) {
+//      Pane reviewPane = new Pane();
+//      int reviewPaneOffset = 100;
 //
-//      Class.forName(Credentials.getDriver());
-//      Connection reviewConnection = DriverManager.getConnection(URL);
-//      Statement statement = reviewConnection.createStatement();
-//      ResultSet resultSet = statement.executeQuery(check_For_Table_SQL);
+//      Label userName = new Label();
+//      Label reviewLabel = new Label();
+//      Label userRating = new Label();
 //
-//      if (resultSet.next()) {
-//        Statement stmt = reviewConnection.createStatement();
-//        stmt.executeQuery(create_Review_SQL);
+//      reviewPane.setPrefHeight(100);
 //
-//      } else {
-//        Statement stmt = reviewConnection.createStatement();
-//        stmt.executeQuery(create_Table_SQL);
-//      }
+//      // set fields of preview window to those from each hotel object
+//      Text reviewText = new Text(review.getReview());
+//      Text userRatingText = new Text(Integer.toString(review.getUserRating()));
+//      Text userNameText = new Text(review.getUsername());
 //
-//    } catch (SQLException ex) {
-//      System.out.println(ex);
-//    }
+//      userName.setText(userNameText.getText());
+//      reviewLabel.setText(reviewText.getText());
+//      userRating.setText(userRatingText.getText());
+//
+//      // configure layout of elements on review list
+//      int xPadding = 100;
+//
+//      userName.setLayoutX(0);
+//      userName.setLayoutY(0);
+//      userRating.setLayoutX(userName.getLayoutX() + xPadding);
+//      userRating.setLayoutY(userName.getLayoutY());
+//      reviewLabel.setLayoutX(userRating.getLayoutX() + xPadding);
+//      reviewLabel.setLayoutY(userRating.getLayoutY());
+//
+//      reviewPane.getChildren().addAll(userName, reviewLabel, userRating);
+//      reviewPane.setLayoutY(reviewPane.getPrefHeight() * reviewPaneOffset);
+//      reviewPane.setPrefWidth(userName.getPrefWidth() + userRating.getPrefWidth() +
+//          reviewLabel.getPrefWidth());
+//
+//      reviewList.getChildren().add(reviewPane);
+//      reviewPaneOffset++;
   }
+
 
   public void backButton(ActionEvent event) throws Exception {
     Navigator.hotelInfo(event);
